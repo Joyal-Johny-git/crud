65505fca4b0b7b35acbf75c4f49e4db364fca59d
Joyal-Johny-git <joyaljohny1529@gmail.com>
2024-06-24 20:32:31 +0530

    first commit

---
+++ Controllers/DataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Crud.Models;
+namespace MVC_Crud.Controllers
+{
+    public class DataController : Controller
+    {
+        Datacontext db = new Datacontext();
+        // GET: Data
+        public ActionResult Index()
+        {
+            List<Data> data = new List<Data>();
+            data = db.datas.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Data data = new Data();
+            db.datas.Add(data);
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create( Data data)
+        {
+            if(ModelState.IsValid)
+            {
+                db.datas.Add(data);
+                db.SaveChanges();
+                Response.Redirect("index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Crud.Models
+{
+    [Table("Data_table")]
+    public class Data
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        public string Department { get; set; }
+        public int Phone { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        
+    }
+}
